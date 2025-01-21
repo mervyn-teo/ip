8ad6b56cac70a5e33e9b61208dfd37dc06bea49d
mervyn-teo <teomervyn@yahoo.com>
2025-01-22 03:13:46 +0800

    Made the echo functionality

--- src/main/java/Skibidi.java
+++ src/main/java/Skibidi.java
@@ -1,14 +1,23 @@
+import java.util.Scanner;
+
 public class Skibidi {
     public static void main(String[] args) {
         String greet = """
-                ____________________________________________________________
-                 Hello! I'm Mr. Skibidi
-                 Have you Skibidied today?
-                ____________________________________________________________
-                 Skibidi bop bop!
-                ____________________________________________________________
-                                
+                    ____________________________________________________________
+                     Hello! I'm Mr. Skibidi
+                     Have you Skibidied today?
+                    ____________________________________________________________                                
                 """;
+        String bye = """
+                    ____________________________________________________________
+                     Skibidi bop bop!
+                    ____________________________________________________________
+                """;
+        String spacer = "    ____________________________________________________________";
         System.out.println(greet);
+        Scanner in = new Scanner(System.in);
+        String userChoice = in.nextLine();
+        System.out.println(spacer + "\n     " + userChoice + "\n" + spacer);
+        System.out.println(bye);
     }
 }
