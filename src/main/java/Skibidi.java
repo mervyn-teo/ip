8e0d3497b413ccce9187af69f42d9aeb6b18297f
mervyn-teo <teomervyn@yahoo.com>
2025-01-22 03:05:02 +0800

    Made initial skeleton for chatbot.

---
+++ src/main/java/Skibidi.java
@@ -0,0 +1,14 @@
+public class Skibidi {
+    public static void main(String[] args) {
+        String greet = """
+                ____________________________________________________________
+                 Hello! I'm Mr. Skibidi
+                 Have you Skibidied today?
+                ____________________________________________________________
+                 Skibidi bop bop!
+                ____________________________________________________________
+                                
+                """;
+        System.out.println(greet);
+    }
+}
