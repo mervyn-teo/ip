5ac1c917a40eb04c0b4a72828ba041b1a5504f3d
mervyn-teo <teomervyn@yahoo.com>
2025-01-23 20:51:16 +0800

    Resolved some conflict and added errors.

--- src/main/java/Skibidi.java
+++ src/main/java/Skibidi.java
@@ -19,56 +19,98 @@ public class Skibidi {
             String userChoice = in.nextLine();
             System.out.println(spacer);
             switch (userChoice) {
-                case "bye":
+                case "bye": {
                     System.out.println(bye);
                     System.out.println(spacer);
                     return;
-                case "list":
-                    for (int i = 0; i < listItems.size(); i++) {
-                        Task item = listItems.get(i);
-                        System.out.println("     " + (i+1) + "." + item );
-                    }
-                    break;
-                default:
+                }
+                case "list": {
+                        if (listItems.isEmpty()) {
+                            System.out.println("There are no item in your list skid skid!");
+                        } else {
+                            for (int i = 0; i < listItems.size(); i++) {
+                                Task item = listItems.get(i);
+                                System.out.println("     " + (i + 1) + "." + item);
+                            }
+                        }
+                        break;
+                }
+                default: {
                     String[] splitUserchoice = userChoice.split(" ", 2);
-
-                    switch (splitUserchoice[0]){
-                        case "mark": {
-                            Task taskItem = listItems.get(Integer.parseInt(splitUserchoice[1]) - 1);
-                            taskItem.markDone();
-                            System.out.println("     Yes this is marked as done skibidi yes yes\n     " + taskItem);
-                            break;
+                    switch (splitUserchoice[0]) {
+                            case "mark": {
+                                if (splitUserchoice.length != 2){
+                                    System.out.println("     Double check what you want from me skibidi bop bop!");
+                                    break;
+                                }
+                                if (Integer.parseInt(splitUserchoice[1]) > listItems.size() | Integer.parseInt(splitUserchoice[1]) <= 0) {
+                                    System.out.println("     This is out of bounds my skibidi!");
+                                } else {
+                                    Task taskItem = listItems.get(Integer.parseInt(splitUserchoice[1]) - 1);
+                                    taskItem.markDone();
+                                    System.out.println("     Yes this is marked as done skibidi yes yes\n     " + taskItem);
+                                }
+                                break;
+                            }
+                            case "unmark": {
+                                if (splitUserchoice.length != 2){
+                                    System.out.println("     Double check what you want from me skibidi bop bop!");
+                                    break;
+                                }
+                                if (Integer.parseInt(splitUserchoice[1]) > listItems.size() | Integer.parseInt(splitUserchoice[1]) <= 0) {
+                                    System.out.println("     This is out of bounds my skibidi!");
+                                } else {
+                                    Task taskItem = listItems.get(Integer.parseInt(splitUserchoice[1]) - 1);
+                                    taskItem.markUndone();
+                                    System.out.println("     skibidi this is marked as undone skibidi bop bop\n     " + taskItem);
+                                }
+                                break;
+                            }
+                            case "todo": {
+                                // expects no time
+                                if (splitUserchoice.length == 1) {
+                                    System.out.println("     Empty todos are not very skibidi!");
+                                } else {
+                                    listItems.add(new toDo(splitUserchoice[1]));
+                                    System.out.println("     added: " + listItems.get(listItems.size() - 1) + "\n     there are " + listItems.size() + " tasks in the list now");
+                                }
+                                break;
+                            }
+                            case "event": {
+                                // expects 1 from and  1 to
+                                if (splitUserchoice.length != 2){
+                                    System.out.println("     no name no date? bop bop!");
+                                    break;
+                                }
+                                String[] splitted = splitUserchoice[1].split("/from | /to ", 3);
+                                if (splitted.length != 3) {
+                                    System.out.println("     Double check what you want from me skibidi bop bop!");
+                                } else {
+                                    listItems.add(new Event(splitted[0], splitted[1], splitted[2]));
+                                    System.out.println("     added: " + listItems.get(listItems.size() - 1) + "\n     there are " + listItems.size() + " tasks in the list now");
+                                }
+                                break;
+                            }
+                            case "deadline": {
+                                // expects 1 deadline time
+                                if (splitUserchoice.length != 2){
+                                    System.out.println("     no name no date? bop bop!");
+                                    break;
+                                }
+                                String[] splitTaskTime = splitUserchoice[1].split("/by ", 2);
+                                if (splitTaskTime.length != 2) {
+                                    System.out.println("     Double check what you want from me skibidi bop bop!");
+                                } else {
+                                    listItems.add(new Deadline(splitTaskTime[0], splitTaskTime[1]));
+                                    System.out.println("     added: " + listItems.get(listItems.size() - 1) + "\n     there are " + listItems.size() + " tasks in the list now");
+                                }
+                                break;
+                            }
+                            default: {
+                                System.out.println("     I dont understand what you mean my dear skibidi");
+                            }
                         }
-                        case "unmark": {
-                            Task taskItem = listItems.get(Integer.parseInt(splitUserchoice[1]) - 1);
-                            taskItem.markUndone();
-                            System.out.println("     skibidi this is marked as undone skibidi bop bop\n     " + taskItem);
-                            break;
-                        }
-                        case "todo": {
-                            // expects no time
-                            listItems.add(new toDo(splitUserchoice[1]));
-                            System.out.println("     added: " + listItems.get(listItems.size() - 1) + "\n     there are " + listItems.size() + " tasks in the list now");
-                            break;
-                        }
-                        case "event": {
-                            // expects 1 from and  1 to
-                            String[] splitted = splitUserchoice[1].split("/from | /to ", 3);
-                            listItems.add(new Event(splitted[0], splitted[1], splitted[2]));
-                            System.out.println("     added: " + listItems.get(listItems.size() - 1) + "\n     there are " + listItems.size() + " tasks in the list now");
-                            break;
-                        }
-                        case "deadline": {
-                            // expects 1 deadline time
-                            String[] splitTaskTime = splitUserchoice[1].split("/by ", 2);
-                            listItems.add(new Deadline(splitTaskTime[0], splitTaskTime[1]));
-                            System.out.println("     added: " + listItems.get(listItems.size() - 1) + "\n     there are " + listItems.size() + " tasks in the list now");
-                            break;
-                        }
-                        default: {
-                        }
-                    }
-
+                }
             }
             System.out.println(spacer);
         }
