94305fb28563c25916302be869e79915ecea4b5f
mervyn-teo <teomervyn@yahoo.com>
2025-01-28 02:02:40 +0800

    Add javadocs for Parser.java

--- src/main/java/skibidi/command/Parser.java
+++ src/main/java/skibidi/command/Parser.java
@@ -1,6 +1,15 @@
 package skibidi.command;
 
+/**
+ * The {@code Parser} class is responsible for parsing user input commands and converting them into
+ * corresponding {@link commandType} enums. It interprets raw text input and executes corresponding
+ * command execution logics.
+ */
 public class Parser {
+
+    /**
+     * Represents the types of commands that can be parsed and processed.
+     */
     public enum commandType {
         LIST,
         BYE,
@@ -13,6 +22,13 @@ public class Parser {
         UNKNOWN
     }
 
+    /**
+     * Parses a raw string input and determines the corresponding {@link commandType}.
+     *
+     * @param s the user input string to be parsed
+     * @return the {@link commandType} corresponding to the string input. If the input is null,
+     *         empty, or does not match any known command, {@code commandType.UNKNOWN} is returned.
+     */
     public static commandType parse(String s) {
         if (s == null || s.isBlank()) {
             return commandType.UNKNOWN;
