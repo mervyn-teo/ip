a0603c80614b542e2177b2c5d46ecb7e488c9eaf
mervyn-teo <teomervyn@yahoo.com>
2025-02-19 17:51:30 +0800

    Rename title and add icon

--- src/main/java/skibidi/gui/Main.java
+++ src/main/java/skibidi/gui/Main.java
@@ -3,6 +3,7 @@ package skibidi.gui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -17,6 +18,8 @@ public class Main extends Application {
     public void start(Stage stage) {
 
         try {
+            stage.setTitle("Skibidi");
+            stage.getIcons().add(new Image("images/skibidi.jpg"));
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
